d026baf280aeec04de5d687586f2281fecec9481
Ali <AliNazarii7394@gmail.com>
2023-10-08 13:27:32 +0330

    product cart created and added to Productlist Page

--- src/components/ProductCart.jsx
+++ src/components/ProductCart.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 
-const ProductsCart = () => {
+const ProductsCart = ({ item }) => {
   return (
-    <div className='cartWrapper d-flex justify-content-between'>
-      <img src='' alt='' />
-      <div className='cartInfo'>
-        <h3 className='title'></h3>
-        <p className='desc'></p>
-        <h3 className='price'></h3>
+    <div class='col-lg-3 mb-4'>
+      <div class='card'>
+        <div className='d-flex justify-content-center pt-5'>
+          <img
+            src={item?.image}
+            alt=''
+            className='card-img-top'
+            style={{ width: "100px", height: "100px" }}
+          />
+        </div>
+        <div class='card-body'>
+          <h5 class='card-title'>{item?.title.slice(0, 10)}</h5>
+          <p class='card-text'>{item?.description.slice(0, 90)} ....</p>
+          <a href='' class='btn btn-outline-success btn-sm w-100'>
+            Add more
+          </a>
+        </div>
       </div>
-      <button className='button'>Add to Cart</button>
     </div>
   );
 };
